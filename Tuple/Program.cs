394a66c0d22df9bc11a9a9c53a87b0da85f85719
Ashutosh <ashutosh.chaturvedi@euromonitor.com>
2022-09-17 01:54:28 +0530

    tuple

---
+++ Tuple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Tuple.Create(1, 2, 3, 4, 5, 6, 7, 8);
+
+
+            Console.WriteLine(numbers.Item1);
+            Console.WriteLine(numbers.Item2);
+            Console.WriteLine(numbers.Item3);
+            Console.WriteLine(numbers.Item4);
+            Console.WriteLine(numbers.Item5);
+            Console.WriteLine(numbers.Item6);
+            Console.WriteLine(numbers.Item7);
+            Console.WriteLine(numbers.Rest);
+            Console.WriteLine(numbers.Rest.Item1);
+
+            Console.ReadLine();
+        }
+    }
+}
